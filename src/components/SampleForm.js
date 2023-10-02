385698c57a93a44dbe7aed810a3bc1426d33cf85
Connor Ford <connor.ford@theleadingpath.com>
2023-10-02 12:23:39 -0400

    Add in numeric validation

--- src/components/SampleForm.js
+++ src/components/SampleForm.js
@@ -1,12 +1,11 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { DefaultValuesContext } from '../context/DefaultValuesContext';
-import InputField from './InputField';
 import '../styles/SampleForm.css';
 
 const SampleForm = () => {
   const { defaultValues, loading, error } = useContext(DefaultValuesContext);
-  const { register, handleSubmit, errors, reset, watch } = useForm({
+  const { register, handleSubmit, reset, watch, setValue, formState: { errors } } = useForm({
     defaultValues: defaultValues
   });
 
@@ -17,70 +16,120 @@ const SampleForm = () => {
     // Here is where we would handle passing the data back to the server
   };
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  useEffect(() => {
+    if (!loading) {
+      for (const key in defaultValues) {
+        setValue(key, defaultValues[key]);
+      }
+    }
+  }, [defaultValues, loading, setValue]);
 
   return (
     <div className="form-container">
       {loading && <p>Loading...</p>}
       {error && <p className="error">{error}</p>}
       <form onSubmit={handleSubmit(onSubmit)}>
-        <InputField
-          label="Sample size:"
-          type="number"
-          name="sample_size"
-          register={register}
-          validation={{
-            required: "Sample size is required",
-            validate: value => (value >= 2 && Number.isInteger(parseFloat(value))) || "Sample size must be a whole number >= 2"
-          }}
-          errorMessage={errors && errors.sample_size?.message}
-        />
-        <InputField
-          label="Sample Mean:"
-          type="text"
-          name="sample_mean"
-          register={register}
-          validation={{
-            required: "Sample mean is required"
-          }}
-          errorMessage={errors && errors.sample_mean?.message}
-        />
-        <InputField
-          label="Standard Deviation:"
-          type="text"
-          name="standard_deviation"
-          register={register}
-          validation={{
-            required: "Standard deviation is required",
-            validate: value => (value > 0) || "Standard deviation must be > 0"
-          }}
-          errorMessage={errors && errors.standard_deviation?.message}
-        />
         <div className="form-group">
-          <label className="label">
-            <input
-              type="checkbox"
-              name="perform_hypothesis"
-              {...register("perform_hypothesis")}
-            />
-            Perform hypothesis test
-          </label>
+          <div className="label-container">
+            {errors.sample_size && <div className="error">{errors.sample_size.message}</div>}
+            <label className="label">Sample size:</label>
+          </div>
+          <input
+            type="number"
+            name="sample_size"
+            data-testid="sample_size"
+            {...register("sample_size", {
+              required: "Sample size is required",
+              pattern: {
+                value: /^[0-9]*(\.[0-9]+)?$/,
+                message: "Must be a valid numeric value"
+              },
+              validate: value => (value >= 2 && Number.isInteger(parseFloat(value))) || "Sample size must be a whole number >= 2"
+            })}
+            className={`input ${errors.sample_size ? 'input-error' : ''}`}
+          />
         </div>
-        <InputField
-          label="Hypothesized mean:"
-          type="text"
-          name="hypothesized_mean"
-          register={register}
-          validation={{
-            required: "Hypothesized mean is required"
-          }}
-          disabled={!performHypothesisTest}
-          errorMessage={errors && errors.hypothesized_mean?.message}
-        />
-        <button type="submit">OK</button>
-        <button type="button" onClick={() => reset(defaultValues)}>Reset</button>
+
+        <div className="form-group">
+          <div className="label-container">
+            {errors.sample_mean && <div className="error">{errors.sample_mean.message}</div>}
+            <label className="label">Sample Mean:</label>
+          </div>
+          <input
+            type="text"
+            name="sample_mean"
+            data-testid="sample_mean"
+            {...register("sample_mean", {
+              required: "Sample mean is required",
+              pattern: {
+                value: /^[0-9]*(\.[0-9]+)?$/,
+                message: "Must be a valid numeric value"
+              }
+            })}
+            className={`input ${errors.sample_mean ? 'input-error' : ''}`}
+          />
+        </div>
+
+        <div className="form-group">
+          <div className="label-container">
+            {errors.standard_deviation && <div className="error">{errors.standard_deviation.message}</div>}
+            <label className="label">Standard Deviation:</label>
+          </div>
+          <input
+            type="text"
+            name="standard_deviation"
+            data-testid="standard_deviation"
+            {...register("standard_deviation", {
+              required: "Standard deviation is required",
+              pattern: {
+                value: /^[0-9]*(\.[0-9]+)?$/,
+                message: "Must be a valid numeric value"
+              },
+              validate: value => (value > 0) || "Standard deviation must be > 0"
+            })}
+            className={`input ${errors.standard_deviation ? 'input-error' : ''}`}
+          />
+        </div>
+
+        <div className="form-group">
+          <div className="label-container">
+            <label className="label">
+              <input
+                type="checkbox"
+                name="perform_hypothesis"
+                data-testid="perform_hypothesis"
+                {...register("perform_hypothesis")}
+              />
+              Perform hypothesis test
+            </label>
+          </div>
+        </div>
+
+        <div className="form-group hypothesized-input">
+          <div className="label-container">
+            {errors.hypothesized_mean && <div className="error">{errors.hypothesized_mean.message}</div>}
+            <label className={`label ${!performHypothesisTest ? 'disabled' : ''}`}>Hypothesized mean:</label>
+          </div>
+          <input
+            type="text"
+            name="hypothesized_mean"
+            data-testid="hypothesized_mean"
+            {...register("hypothesized_mean", {
+              required: performHypothesisTest ? "Hypothesized mean is required" : false,
+              pattern: {
+                value: /^[0-9]*(\.[0-9]+)?$/,
+                message: "Must be a valid numeric value"
+              }
+            })}
+            className={`input ${errors.hypothesized_mean ? 'input-error' : ''}`}
+            disabled={!performHypothesisTest}
+          />
+        </div>
+
+        <div className="button-group">
+          <button type="submit" className="button-ok">OK</button>
+          <button type="button" onClick={() => reset(defaultValues)} className="button-reset">Reset</button>
+        </div>
       </form>
     </div>
   );
